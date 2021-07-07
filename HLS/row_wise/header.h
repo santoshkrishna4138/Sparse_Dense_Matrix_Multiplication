3052b58b5798d109f9d3378182647caa29e5e366
srinivasans74 <cool.srini97@gmail.com>
2021-07-07 16:56:20 +0530

    commit

--- HLS/row_wise/header.h
+++ HLS/row_wise/header.h
@@ -4,6 +4,5 @@
 #include<hls_stream.h>
 
 void kernelrow(int a1[(dim*dim)/2], int a2[(dim*dim)/2],int out1[(dim*dim)/2], int out2[(dim*dim)/2]);
-
 void mmstream(hls::stream<int>&b1,hls::stream<int>&b2,hls::stream<ap_uint<64>>&out1,
 	hls::stream<ap_uint<64>>&out2);
--- HLS/row_wise/test.cpp
+++ HLS/row_wise/test.cpp
@@ -15,7 +15,7 @@ int out2[(dim*dim)/2];
 
 int B[dim][dim];
 int A[dim][dim];
-
+int Cdense[dim][dim];
 int rowweight[dim];
 
 int colptr[non_null*dim];
@@ -26,13 +26,12 @@ int*bp2;
 
 hls::stream<int>sparsestreamb1 ("dense matr1");
 hls::stream<int>sparsestreamb2 ("dense matr2");
-
 hls::stream<ap_uint<64>>outstream1 ("Outputstream 1");
 hls::stream<ap_uint<64>>outstream2 ("Outputstream 2");
 
 void row_weight()
 {
-	
+
 ifstream aaaa;
 aaaa.open("D:/Work/PES/ASAP/symbols.txt");
 int num;
@@ -56,7 +55,7 @@ for(int j=0;j<dim;j++)
 
 
 aaaa.close();
-int count2=0;	
+int count2=0;
 for(int i=0;i<dim;i++)
 {
 	int rowcount=0;
@@ -65,58 +64,136 @@ for(int i=0;i<dim;i++)
 
 			if(A[i][j]!=0)
 			{
-				
+
 				val[count2]=A[i][j];
 				colptr[count2]=j;
 				count2++;
 				rowcount++;
-				
+
 			}
-	
+
 	}
 	rowweight[i]=rowcount;
-	
-}	
 
-	std::cout<<"done with sparse \n";
-}	
-
-
-
-
-
-
-
-void tosparse()
-{
-int iq,p,q,qx,k,pp;
-
-for(int i=0;i<dim;i++)
-{
-	
-	int rowintr=non_null;
-		for (int kk=0;kk<non_null;kk++)
-	{
-		
-		for (int k=0;k<(dim/2);k++)
-		
-		{
-		sparsestreamb1.write(b1[(dim/2)*colptr[(kk+qx)]+k]);
-	//	std::cout<<"C1"<<"["<<qx+kk<<"]"<<"="<<"val ["<<k+pp<<"]"<<"*"<<"b1["<<((dim/2)*csc[(kk+pp)]+k)<<"]"<<"\n";
-		sparsestreamb2.write((b2[(dim/2)*colptr[(kk+qx)]+k]));
-	//	std::cout<<"C2"<<"["<<kk+qx<<"]"<<"="<<"val ["<<k+pp<<"]"<<"*"<<"b2["<<((dim/2)*csc[(kk+pp)]+k)<<"]"<<"\n";
-		}
-		
-	//	std::cout<<"\n";
-		
-	}	
-	pp+=(dim/2);
-	qx+=non_null;
 }
 
-std::cout<<"\n Sparse "<<" done \n";
 
-}	
+	std::cout<<"done with sparse \n";
+
+
+	ofstream value;
+	value.open("D:/Work/PES/ASAP/val.txt");
+	for (int i=0;i<(dim*non_null);i++)
+	{
+
+	value<<val[i]<<",";
+	}
+
+	value.close();
+
+}
+
+
+
+
+
+void cdense()
+{
+
+	for (int i=0;i<dim;i++)
+	{
+		for(int j=0;j<dim;j++)
+		{
+			for (int k=0;k<dim;k++)
+			{
+
+				Cdense[i][k]+=A[i][j]*B[j][k];
+
+		    }
+
+	 }
+	}
+
+
+}
+
+void cdenseprint()
+{
+	ofstream dense;
+	dense.open("D:/Work/PES/ASAP/CPUdense.txt");
+
+
+
+	for (int i=0;i<dim;i++)
+	{
+		for(int k=0;k<dim;k++)
+		{
+			dense<<Cdense[i][k]<<"\n";
+
+		}
+	}
+
+	dense.close();
+
+
+}
+
+
+
+void ctoprint()
+{
+	ofstream coua;
+	coua.open("D:/Work/PES/ASAP/FPGa.txt");
+
+	for (int i=0;i<(dim*dim)/2;i++)
+	{
+
+	coua<<out1[i]<<"\n"<<out2[i]<<"\n";
+	}
+	coua.close();
+
+}
+
+
+
+void cstream()
+{
+
+	int p,q,qx,k,pp;
+
+	for(int i=0;i<dim;i++)
+	{
+
+		int rowintr=non_null;
+			for (int kk=0;kk<non_null;kk++)
+		{
+
+			for (int k=0;k<(dim/2);k++)
+
+			{
+			sparsestreamb1.write(b1[(dim/2)*colptr[(kk+qx)]+k]);
+		//	std::cout<<"C1"<<"["<<qx+kk<<"]"<<"="<<"val ["<<k+pp<<"]"<<"*"<<"b1["<<((dim/2)*csc[(kk+pp)]+k)<<"]"<<"\n";
+			sparsestreamb2.write((b2[(dim/2)*colptr[(kk+qx)]+k]));
+		//	std::cout<<"C2"<<"["<<kk+qx<<"]"<<"="<<"val ["<<k+pp<<"]"<<"*"<<"b2["<<((dim/2)*csc[(kk+pp)]+k)<<"]"<<"\n";
+			}
+
+		//	std::cout<<"\n";
+
+		}
+		pp+=(dim/2);
+		qx+=non_null;
+	}
+
+	std::cout<<"\n Sparse "<<" done \n";
+
+
+
+
+
+
+
+}
+
 
 
 
@@ -158,29 +235,18 @@ bp1=b1;
 bp2=b2;
 
 row_weight();
-tosparse();
 
 
+cdense();
+cdenseprint();
 
-
+cstream();
+kernelrow(bp1,bp2,out1,out2);
 mmstream(sparsestreamb1,sparsestreamb2,outstream1,outstream2);
 
-//kernelrow(bp1,bp2,out1,out2);
-
-
-
-
+ctoprint();
 ofstream combinedc1andc2;
-combinedc1andc2.open("D:/Work/PES/ASAP/streamkernel.txt");
-
-
-/*
-for (int i=0;i<(dim*dim)/2;i++)
-{
-
-combinedc1andc2<<out1[i]<<"\n"<<out2[i]<<"\n";
-}
-*/
+combinedc1andc2.open("D:/Work/PES/ASAP/cstreamfinalval.txt");
 
 for (int i=0;i<(dim*dim)/2;i++)
 {
@@ -192,6 +258,7 @@ combinedc1andc2<<outstream1.read()<<"\n"<<outstream2.read()<<"\n";
 
 
 combinedc1andc2.close();
+
 
 
 return 0;
